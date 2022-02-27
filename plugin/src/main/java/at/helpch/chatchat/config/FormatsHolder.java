a32bbb5ed1ba22805730d5fa5a7d9d5a66ca4a7c
iGabyTM <contactgabytm@gmail.com>
2022-02-27 10:53:32 +0200

    [plugin] add basic implementation for chat formats

--- plugin/src/main/java/at/helpch/chatchat/config/FormatsHolder.java
+++ plugin/src/main/java/at/helpch/chatchat/config/FormatsHolder.java
@@ -1,7 +1,15 @@
 package at.helpch.chatchat.config;
 
+import at.helpch.chatchat.format.ChatFormat;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ConfigSerializable
 public final class FormatsHolder {
+
+    private String defaultFormat = "default";
+    private Map<String, ChatFormat> formats = new HashMap<>();
+
 }
