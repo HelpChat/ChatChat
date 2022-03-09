2fb754a012e3be99353c2219aaa914b22a17b13c
Kqliber <kqliber@gmail.com>
2022-03-09 17:45:37 +0000

    made channels and formats actually serializable

--- plugin/src/main/java/at/helpch/chatchat/format/ChatFormat.java
+++ plugin/src/main/java/at/helpch/chatchat/format/ChatFormat.java
@@ -2,10 +2,12 @@ package at.helpch.chatchat.format;
 
 import at.helpch.chatchat.api.Format;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
 import java.util.Collections;
 import java.util.List;
 
+@ConfigSerializable
 public class ChatFormat implements Format {
 
     private int priority = Integer.MAX_VALUE;
