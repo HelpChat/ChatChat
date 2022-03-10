ec3c45f1652a2dcd4277f8a1613b0e49a18d943f
Kqliber <kqliber@gmail.com>
2022-03-10 22:34:38 +0000

    added a permission check

--- plugin/src/main/java/at/helpch/chatchat/command/WhisperCommand.java
+++ plugin/src/main/java/at/helpch/chatchat/command/WhisperCommand.java
@@ -6,12 +6,15 @@ import at.helpch.chatchat.util.FormatUtils;
 import dev.triumphteam.cmd.core.BaseCommand;
 import dev.triumphteam.cmd.core.annotation.Command;
 import dev.triumphteam.cmd.core.annotation.Default;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 @Command(value = "whisper", alias = {"tell", "w", "msg", "message"})
 public final class WhisperCommand extends BaseCommand {
 
+    private static final String MESSAGE_PERMISSION = "chatchat.pm";
     private final ChatChatPlugin plugin;
 
     public WhisperCommand(@NotNull final ChatChatPlugin plugin) {
@@ -20,6 +23,11 @@ public final class WhisperCommand extends BaseCommand {
 
     @Default
     public void whisperCommand(final Player sender, final Player target, final String message) {
+        if (!sender.hasPermission(MESSAGE_PERMISSION)) {
+            plugin.audiences().player(sender).sendMessage(Component.text("No permission.", NamedTextColor.RED)); // make this not hardcoded
+            return;
+        }
+
         var settingsConfig = plugin.configManager().settings();
 
         final PMFormat senderFormat;
