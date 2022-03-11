0ebe926d87600a6cbaa6e36f8c2be81b28d2c50e
Kqliber <kqliber@gmail.com>
2022-03-11 20:42:34 +0000

    created a static factory method, added the usersInChannel field

--- plugin/src/main/java/at/helpch/chatchat/channel/ChatChannel.java
+++ plugin/src/main/java/at/helpch/chatchat/channel/ChatChannel.java
@@ -11,16 +11,44 @@ import java.util.List;
 @ConfigSerializable
 public final class ChatChannel implements Channel {
 
-
     private String messagePrefix = "";
 
     private String toggleCommand = "global";
 
     private String channelPrefix = "[global]";
 
+    private transient boolean isDefault = false;
+
+    private transient List<User> usersInChannel;
+
+    // Configurate constructor
+    public ChatChannel() {}
+
+    private ChatChannel(
+            @NotNull final String messagePrefix,
+            @NotNull final String toggleCommand,
+            @NotNull final String channelPrefix,
+            final boolean isDefault,
+            @NotNull final List<User> usersInChannel) {
+        this.messagePrefix = messagePrefix;
+        this.toggleCommand = toggleCommand;
+        this.channelPrefix = channelPrefix;
+        this.isDefault = isDefault;
+        this.usersInChannel = usersInChannel;
+    }
+
+    public static @NotNull ChatChannel of(
+            @NotNull final String messagePrefix,
+            @NotNull final String toggleCommand,
+            @NotNull final String channelPrefix,
+            final boolean isDefault,
+            @NotNull final List<User> usersInChannel) {
+        return new ChatChannel(messagePrefix, toggleCommand, channelPrefix, isDefault, usersInChannel);
+    }
+
     @Override
     public boolean isDefault() {
-        return false;
+        return isDefault;
     }
 
     @Override
