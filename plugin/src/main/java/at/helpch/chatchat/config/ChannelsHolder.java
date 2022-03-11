4a7a75919a880ba7f24fbb104ef52855ab13876f
Kqliber <kqliber@gmail.com>
2022-03-11 20:25:27 +0000

    added default channel + added getters

--- plugin/src/main/java/at/helpch/chatchat/config/ChannelsHolder.java
+++ plugin/src/main/java/at/helpch/chatchat/config/ChannelsHolder.java
@@ -1,14 +1,22 @@
 package at.helpch.chatchat.config;
 
 import at.helpch.chatchat.channel.ChatChannel;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @ConfigSerializable
 public final class ChannelsHolder {
 
     private String defaultChannel = "default";
-    private Map<String, ChatChannel> channels = new HashMap<>();
+    private Map<String, ChatChannel> channels = Map.of("default", new ChatChannel());
+
+    public @NotNull String defaultChannel() {
+        return defaultChannel;
+    }
+
+    public @NotNull Map<String, ChatChannel> channels() {
+        return Map.copyOf(channels);
+    }
 }
