83a44dc9ec0309b802cc155ac0d3832ba1481564
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-04-26 18:18:27 +0300

    Initial work on MentionEvent Moved the mention processors to the MentionProcessor util class

---
+++ api/src/main/java/at/helpch/chatchat/api/event/MentionEvent.java
@@ -0,0 +1,67 @@
+package at.helpch.chatchat.api.event;
+
+import at.helpch.chatchat.api.Channel;
+import at.helpch.chatchat.api.ChatUser;
+import at.helpch.chatchat.api.MentionType;
+import at.helpch.chatchat.api.User;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class MentionEvent extends Event implements Cancellable {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+    private boolean cancelled = false;
+
+    private @NotNull final ChatUser user;
+    private @NotNull final User target;
+    private @NotNull final Channel channel;
+
+    private @NotNull final MentionType type;
+
+    public static @NotNull HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    /**
+     * Called when a {@link ChatUser} mentions another {@link User}.
+     */
+    public MentionEvent(@NotNull final ChatUser user, @NotNull final User target, @NotNull final Channel channel, @NotNull final MentionType type) {
+        this.user = user;
+        this.target = target;
+        this.channel = channel;
+        this.type = type;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(final boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    public @NotNull ChatUser user() {
+        return user;
+    }
+
+    public @NotNull User target() {
+        return target;
+    }
+
+    public @NotNull Channel channel() {
+        return channel;
+    }
+
+    public MentionType type() {
+        return type;
+    }
+}
