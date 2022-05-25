e5c3ac17b602af3fd55a10df26e78e8a5a4d20e0
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-05-25 16:57:05 +0300

    Only remove Last Messaged User on vanish, if the player can't see the vanished player anymore

--- plugin/src/main/java/at/helpch/chatchat/listener/SuperVanishListener.java
+++ plugin/src/main/java/at/helpch/chatchat/listener/SuperVanishListener.java
@@ -17,12 +17,13 @@ public class SuperVanishListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onVanish(@NotNull final PlayerHideEvent event) {
-        // find everyone who last messaged the person leaving, and remove their reference
+        // find everyone who last messaged the person being vanished, and remove their reference
         plugin.usersHolder().users().stream()
             .filter(user -> user instanceof ChatUser)
             .map(user -> (ChatUser) user)
             .filter(user -> user.lastMessagedUser().isPresent())
             .filter(user -> user.lastMessagedUser().get().player().equals(event.getPlayer()))
+            .filter(user -> !user.canSee(user.lastMessagedUser().get()))
             .forEach(user -> user.lastMessagedUser(null));
     }
 }
