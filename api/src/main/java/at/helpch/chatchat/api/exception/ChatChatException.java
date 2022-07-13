cd2472668d078f8871bc5d7209e9aa48f6847bce
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-07-13 16:08:24 +0300

    Added a version helper

---
+++ api/src/main/java/at/helpch/chatchat/api/exception/ChatChatException.java
@@ -0,0 +1,7 @@
+package at.helpch.chatchat.api.exception;
+
+public final class ChatChatException extends RuntimeException {
+    public ChatChatException(String message) {
+        super(message);
+    }
+}
