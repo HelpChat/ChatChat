c6287a50f2af0051439a2196ee1901c1f6e18245
Gabriel Dumitru <gabriel.dumitru@rinf.tech>
2022-08-01 20:44:08 +0300

    improve the <item> placeholder

--- plugin/src/main/java/at/helpch/chatchat/util/ItemUtils.java
+++ plugin/src/main/java/at/helpch/chatchat/util/ItemUtils.java
@@ -1,21 +1,23 @@
 package at.helpch.chatchat.util;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.stream.Collectors;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class ItemUtils {
     private static final LegacyComponentSerializer LEGACY_COMPONENT_SERIALIZER = LegacyComponentSerializer.legacyAmpersand();
@@ -25,68 +27,59 @@ public final class ItemUtils {
     }
 
     public static @NotNull TagResolver.@NotNull Single createItemPlaceholder(
-        @NotNull final String itemFormat,
-        @NotNull final String itemFormatInfo,
-        @NotNull final ItemStack item
+            @NotNull final String itemFormat,
+            @NotNull final String itemFormatInfo,
+            @NotNull final ItemStack item
     ) {
-        final var itemPlaceholder = Placeholder.component(
-            "item",
-            Component.text(item.getType().name().toLowerCase(Locale.getDefault()))
-        );
-
-        final var amountPlaceholder = Placeholder.component(
-            "amount",
-            Component.text(item.getAmount())
-        );
+        final var materialName = Component.translatable("item.minecraft." + item.getType().getKey().getKey());
+        final var itemPlaceholder = Placeholder.component("item", materialName);
+        final var amountPlaceholder = Placeholder.component("amount", Component.text(item.getAmount()));
 
         final var hoverInfoComponent = !itemFormatInfo.isBlank()
-            ? MessageUtils.parseToMiniMessage(itemFormatInfo, itemPlaceholder, amountPlaceholder)
-            : null;
+                ? MessageUtils.parseToMiniMessage(itemFormatInfo, itemPlaceholder, amountPlaceholder)
+                : null;
 
         if (item.getType() == Material.AIR ||
-            item.getType() == Material.CAVE_AIR ||
-            item.getType() == Material.VOID_AIR ||
-            item.getType() == Material.LEGACY_AIR ||
-            !item.hasItemMeta()
+                item.getType() == Material.CAVE_AIR ||
+                item.getType() == Material.VOID_AIR ||
+                item.getType() == Material.LEGACY_AIR ||
+                !item.hasItemMeta()
         ) {
             return Placeholder.component(
-                "item",
-                MessageUtils.parseToMiniMessage(itemFormat, itemPlaceholder, amountPlaceholder).hoverEvent(hoverInfoComponent));
+                    "item",
+                    MessageUtils.parseToMiniMessage(itemFormat, itemPlaceholder, amountPlaceholder).hoverEvent(hoverInfoComponent)
+            );
         }
 
         final var meta = item.getItemMeta();
 
-        final var name = LEGACY_COMPONENT_SERIALIZER.deserialize(
-            meta.hasDisplayName()
-                ? meta.getDisplayName()
-                : meta.hasLocalizedName()
-                ? meta.getLocalizedName()
-                : item.getType().name().toLowerCase(Locale.getDefault())
-        );
+        // To get rid of IDE warnings
+        if (meta == null) {
+            return Placeholder.component(
+                    "item",
+                    MessageUtils.parseToMiniMessage(itemFormat, itemPlaceholder, amountPlaceholder).hoverEvent(hoverInfoComponent)
+            );
+        }
+
+        final Component name = meta.hasDisplayName() ? LEGACY_COMPONENT_SERIALIZER.deserialize(meta.getDisplayName()) : materialName;
 
         final var newItemPlaceholder = Placeholder.component("item", name);
 
         final List<Component> enchants = meta.hasEnchants() && !meta.hasItemFlag(ItemFlag.HIDE_ENCHANTS)
-            ? meta.getEnchants()
-            .entrySet()
-            .stream()
-            .map(entry -> "&7" + formattedEnchantment(entry))
-            .map(LEGACY_COMPONENT_SERIALIZER::deserialize)
-            .collect(Collectors.toList())
-            : Collections.emptyList();
+                ? meta.getEnchants()
+                .entrySet()
+                .stream()
+                .map(entry -> formattedEnchantment(entry.getKey(), entry.getValue()).color(NamedTextColor.GRAY))
+                .collect(Collectors.toList())
+                : Collections.emptyList();
 
         final List<Component> lore = meta.hasLore()
-            ? meta.getLore()
-            .stream()
-            .map(LEGACY_COMPONENT_SERIALIZER::deserialize)
-            .map(textComponent -> {
-                if (!textComponent.hasStyling()) {
-                    return textComponent.color(NamedTextColor.DARK_PURPLE);
-                }
-                return textComponent;
-            })
-            .collect(Collectors.toList())
-            : Collections.emptyList();
+                ? meta.getLore()
+                .stream()
+                .map(LEGACY_COMPONENT_SERIALIZER::deserialize)
+                .map(it -> it.hasStyling() ? it : it.color(NamedTextColor.DARK_PURPLE))
+                .collect(Collectors.toList())
+                : Collections.emptyList();
 
         final var hoverComponents = new ArrayList<Component>();
 
@@ -100,36 +93,31 @@ public final class ItemUtils {
         }
 
         return Placeholder.component(
-            "item",
-            MessageUtils.parseToMiniMessage(itemFormat, newItemPlaceholder, amountPlaceholder).hoverEvent(
-                hoverComponents.stream().collect(Component.toComponent(Component.newline()))
-            )
+                "item",
+                MessageUtils.parseToMiniMessage(itemFormat, newItemPlaceholder, amountPlaceholder).hoverEvent(
+                        Component.join(JoinConfiguration.newlines(), hoverComponents)
+                )
         );
     }
 
-    private static @NotNull String formattedEnchantment(@NotNull final Map.Entry<Enchantment, Integer> entry) {
-        final var enchantment = entry.getKey();
-        final var value = entry.getValue();
-
+    private static @NotNull Component formattedEnchantment(@Nullable final Enchantment enchantment, @Nullable final Integer level) {
         if (enchantment == null) {
-            return "";
+            return Component.empty();
         }
 
-        final var key = enchantment.getKey().getKey();
-
-        final var enchantmentName = key.substring(0, 1).toUpperCase(Locale.getDefault())
-            + key.substring(1);
+        final var isVanilla = enchantment.getKey().getNamespace().equals(NamespacedKey.MINECRAFT);
+        final Component enchantmentName = isVanilla ? Component.translatable("enchantment.minecraft." + enchantment.getKey().getKey()) : Component.text(enchantment.getName());
 
         if (enchantment.getMaxLevel() == 1) {
             return enchantmentName;
         }
 
-        if (value == null) {
-            return enchantmentName + " I";
+        if (level == null) {
+            return enchantmentName.append(Component.text(" I"));
         }
 
         @NotNull final String roman;
-        switch (value) {
+        switch (level) {
             case 1:
                 roman = "I";
                 break;
@@ -146,10 +134,10 @@ public final class ItemUtils {
                 roman = "V";
                 break;
             default:
-                roman = value.toString();
+                roman = level.toString();
                 break;
         }
 
-        return enchantmentName + " " + roman;
+        return enchantmentName.append(Component.space()).append(Component.text(roman));
     }
 }
