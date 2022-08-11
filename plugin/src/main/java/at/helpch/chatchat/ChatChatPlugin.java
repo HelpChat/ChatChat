c4562705e214613eae6660740c3c6dc57c41f7ef
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-08-11 20:40:56 +0300

    Added a format test command

--- plugin/src/main/java/at/helpch/chatchat/ChatChatPlugin.java
+++ plugin/src/main/java/at/helpch/chatchat/ChatChatPlugin.java
@@ -30,6 +30,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
